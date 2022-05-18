f8f115b28ce69bbd11fbecb923a4b994fa33d560
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-05-18 22:45:06 +0300

    synchronized

--- src/router/PrivateRouter.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-
-const PrivateRouter = () => {
-  const currentUser = true;
-
-  return currentUser ? <Outlet /> : <Navigate to="/login" />;
-};
-
-export default PrivateRouter;
